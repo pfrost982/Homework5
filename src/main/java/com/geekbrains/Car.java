6ae71793366f43556316294f8e4b8baee0beb1a9
pfrost982 <power982@gmail.com>
2021-03-30 22:01:03 +0700

    Домашняя работа 5

--- src/main/java/com/geekbrains/Car.java
+++ src/main/java/com/geekbrains/Car.java
@@ -1,36 +1,65 @@
 package com.geekbrains;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.Lock;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
+
     static {
         CARS_COUNT = 0;
     }
+
     private Race race;
     private int speed;
     private String name;
+    private CountDownLatch raceReady;
+    private Lock startLock;
+    private CountDownLatch raceOver;
+
     public String getName() {
         return name;
     }
+
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+
+    public Car(Race race, int speed, CountDownLatch raceReady, Lock startLock, CountDownLatch raceOver) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.raceReady = raceReady;
+        this.startLock = startLock;
+        this.raceOver = raceOver;
     }
+
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
-            Thread.sleep(500 + (int)(Math.random() * 800));
+            Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            raceReady.countDown();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        try {
+            startLock.lock();
+        } finally {
+            startLock.unlock();
+        }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
+        }
+        if (!race.isWinnerExist()) {
+            System.out.println(name + " ПОБЕДИТЕЛЬ!!!");
+        }
+        try {
+            raceOver.countDown();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/com/geekbrains/Tunnel.java
+++ src/main/java/com/geekbrains/Tunnel.java
@@ -1,21 +1,29 @@
 package com.geekbrains;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
-    public Tunnel() {
+    private Semaphore tunnelAccess;
+
+    public Tunnel(int permits) {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
+        tunnelAccess = new Semaphore(permits);
     }
+
     @Override
     public void go(Car c) {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                tunnelAccess.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                tunnelAccess.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
